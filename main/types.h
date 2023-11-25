b2d1b91752fb2bd3d0e85e7de6d3e5a5a4bdcd58
João Carlos <doidobr46@gmail.com>
2023-11-25 18:10:40 -0300

    System types developed

---
+++ main/types.h
@@ -0,0 +1,30 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <stdio.h>
+#define CAR_PLATE_LENGTH 9
+
+enum car_status {
+    WAITING_IN_QUEUE_TO_PARK,
+    PARKED,
+    WAITING_IN_QEUE_TO_LEAVE,
+};
+
+enum parking_slot_status {
+    FREE_SLOT,
+    OCCUPIED,
+};
+
+typedef struct car_struct {
+    uint16_t time_parked;
+    uint16_t entrance_time;
+    char plate[CAR_PLATE_LENGTH];
+    uint8_t status;
+} car_t;
+
+typedef struct parking_slot_struct {
+    uint8_t status;
+    car_t parked_car;
+} parking_slot_t;
+
+#endif  // _TYPES_H_
