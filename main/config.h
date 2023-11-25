02ae9b0ba734ef8dbff780f6e62cc73b07168e6c
João Carlos <doidobr46@gmail.com>
2023-11-25 18:11:09 -0300

    Changes to overall system settings

--- main/config.h
+++ main/config.h
@@ -5,10 +5,14 @@
 #include "driver/gpio.h"
 
 //Parking lot settings
-#define PARKING_SPOT_QUANTITY   20
-#define VALUE_PER_TIME    20
+#define PARKING_SPOT_QUANTITY 20
+#define VALUE_PER_TIME 20
+#define GATE_OPEN_TIME  4000
+#define GATE_CLOSE_TIME 2000
+#define CAR_SPEED 500
 
 //GPIO settings
 #define CAR_ENTERED_BTN GPIO_NUM_0
+#define DEBOUNCING_TIME 300
 
 #endif  // _CONFIG_H_
